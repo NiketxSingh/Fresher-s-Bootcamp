7914371302c834d5e810c3b2fc990525851b846c
minaltayal <minaltayal7@gmail.com>
2025-11-08 23:53:34 +0530

    levels added

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -8,7 +8,7 @@ public class HealthMnager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     priva
+     
     }
 
     // Update is called once per frame
